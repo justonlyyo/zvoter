93f9091f0e9daaf3e27462c4008a615df83f9fe6
justonlyyo <583736361@qq.com>
2017-02-28 10:58:36 +0800

    delete zovter_mon

--- zvoter_mon/temp.py
+++
@@ -1,8 +0,0 @@
-import requests
-import pyquery as pq
-
-res = requests.get("http://r.qidian.com/recom")
-if res.status_code == 200:
-    temp_html = res.text
-    obj = pq.PyQuery(temp_html)
-    print(obj.text())
